28ec6588b66c5420f4058fe0e5e62f2b74033c6b
LoeiFy <LoeiFy@gmail.com>
2017-11-29 11:48:55 +0800

    unit and coverage test

--- test/index.js
+++ test/index.js
@@ -1,20 +1,90 @@
+const assert = require('power-assert')
+const fs = require('fs')
+const path = require('path')
 const Fetcher = require('../')
 
-const config = {
+const defaults = {
   user: 'LoeiFy',
   repository: 'Recordum',
   per: 5,
   order: 'created',
-  cache: true
-}
-const fetcher = new Fetcher(config)
-
-fetcher.status = (s) => {
-  console.log(s)
+  cache: false,
+  token: '5#c21bffc137f44faf7e9c4a84da827f7cc2cfeaa'
 }
 
-fetcher.fetch()
-.then(res => console.log(res))
-.catch(err => console.log(err))
+const json = path.join(process.cwd(), '_issues.json')
 
+function rejects(promise) {
+  return promise
+    .then(() => Promise.reject(new Error('Missing expected rejection')))
+    .catch(reason => Promise.resolve(reason))
+}
 
+function getConfig() {
+  return JSON.parse(JSON.stringify(defaults))
+}
+
+describe('processor', () => {
+  it('token error', async function() {
+    this.timeout(10000)
+
+    const config = getConfig()
+    config.token = 'xxx'
+    const fetcher = new Fetcher(config)
+
+    assert((await rejects(fetcher.fetch())).message.indexOf('401') > -1)
+  })
+
+  it('json', async function() {
+    this.timeout(10000)
+
+    const config = getConfig()
+    const fetcher = new Fetcher(config)
+    const messages = []
+
+    fetcher.status = (s) => {
+      messages.push(s)
+    }
+
+    const result = await fetcher.fetch()
+
+    assert(Array.isArray(result) === true)
+    assert(messages.length === Math.ceil(result.length / config.per))
+  })
+
+  it('no token', async function() {
+    this.timeout(10000)
+
+    const config = getConfig()
+    config.token = ''
+    const fetcher = new Fetcher(config)
+
+    try {
+      const result = await fetcher.fetch()
+      assert(Array.isArray(result) === true)
+    } catch (e) {
+      assert(e.message.indexOf('Maximum') > -1)
+    }
+  })
+
+  it('cache', async function() {
+    this.timeout(10000)
+
+    const config = getConfig()
+    config.cache = true
+    const fetcher = new Fetcher(config)
+    let result = await fetcher.fetch()
+
+    assert(fs.existsSync(json) === true)
+
+    let msg = ''
+    fetcher.status = (s) => {
+      msg = s
+    }
+
+    result = await fetcher.fetch()
+
+    assert(Array.isArray(result) === true)
+    assert(msg === 'Data from cache...')
+  })
+})
